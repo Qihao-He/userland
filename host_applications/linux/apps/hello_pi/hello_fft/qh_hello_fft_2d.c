601c4a2b9244c428c69e358d2849dffb349728d1
Qihao He <qi.he@maine.edu>
2018-02-25 14:14:47 -0500

    qh_hello_fft_2d.c debug

--- host_applications/linux/apps/hello_pi/hello_fft/qh_hello_fft_2d.c
+++ host_applications/linux/apps/hello_pi/hello_fft/qh_hello_fft_2d.c
@@ -46,121 +46,129 @@ Author:Qihao He
 
 char Usage[] =
     "Usage: hello_fft.bin log2_N [jobs [loops [RMS_C]]]\n"
-    "log2_N = log2(FFT_length),       log2_N = 8...11\n";
-    // "log2_M = log2(FFT_length),       log2_M > log2_N\n"
-    // "loops  = number of test repeats, loops>0,       default 1\n"
+    "log2_N = log2(FFT_length),       log2_N = 8...11\n"
+    "log2_M = log2(FFT_length),       log2_M > log2_N\n"
+    "loops  = number of test repeats, loops>0,       default 1\n";
     // "RMS_C  = number of test repeats, T(1),F(0),     default 0\n";
 
-unsigned Microseconds(void) {
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    return ts.tv_sec*1000000 + ts.tv_nsec/1000;
-}
+unsigned Microseconds(void);
 
 int main(int argc, char *argv[]) {
-    int x, y, ret, mb = mbox_open(), log2_N, N;
+    int x, y, ret, mb = mbox_open(), log2_N, log2_M, log2_P, span_log2_N, loops,
+     N;
     unsigned t[4];
 
     struct GPU_FFT_COMPLEX *row;
     struct GPU_FFT_TRANS *trans;
     struct GPU_FFT *fft_pass[2];
 
-    log2_N = argc>1? atoi(argv[1]) : 12; // 8 <= log2_N <= 22
-    // log2_M = argc>2? atoi(argv[2]) : log2_N + 1; // 8 <= log2_N <= 22
-    // loops  = argc>4? atoi(argv[4]) : 1;  // test repetitions
-    // RMS_C  = argc>5? atoi(argv[5]) : 1;  // RMS_controller
+    log2_N = argc>1? atoi(argv[1]) : 12; // 8 <= log2_N <= 11
+    log2_M = argc>2? atoi(argv[2]) : log2_N + 1; // 8 <= log2_N <= 11
+    loops  = argc>3? atoi(argv[3]) : 1;  // test repetitions
+    // RMS_C  = argc>5? atoi(argv[4]) : 1;  // RMS_controller
     //
-    // if (!(argc >=2 && argc <= 6) || jobs < 1 || loops < 1 ||
-    // !(RMS_C >= 0 && RMS_C <=1 ) || log2_N >= log2_M ){
-    //     printf(Usage);
-    //     return -1;
-    // }
-
-    if (argc<2) {
+    if (!(argc >=2 && argc <= 4) || loops < 1 || log2_N >= log2_M ||
+    !(log2_N >= 8 && log2_N <= 11)){
         printf(Usage);
         return -1;
     }
 
-    N = 1<<log2_N;
+    span_log2_N = log2_M - log2_N;
+    time_elapsed_init(span_log2_N, loops);
+    // print out lables for .csv file
+    printf("log2_N,N,Init_T,FFT_T,RMS_T,Total_T\n");
 
-    BITMAPFILEHEADER bfh;
-    BITMAPINFOHEADER bih;
+    for(l = 0; l < span_log2_N; l++){
+        log2_P = log2_N + l;
+        N = 1<<log2_P; // FFT length
 
-    // Create Windows bitmap file
-    FILE *fp = fopen("hello_fft_2d.bmp", "wb");
-    if (!fp) return -666;
+        BITMAPFILEHEADER bfh;
+        BITMAPINFOHEADER bih;
 
-    // Write bitmap header
-    memset(&bfh, 0, sizeof(bfh));
-    bfh.bfType = 0x4D42; //"BM"
-    bfh.bfSize = N*N*3;
-    bfh.bfOffBits = sizeof(bfh) + sizeof(bih);
-    fwrite(&bfh, sizeof(bfh), 1, fp);
+        // Create Windows bitmap file
+        FILE *fp = fopen("hello_fft_2d.bmp", "wb");
+        if (!fp) return -666;
 
-    // Write bitmap info
-    memset(&bih, 0, sizeof(bih));
-    bih.biSize = sizeof(bih);
-    bih.biWidth = N;
-    bih.biHeight = N;
-    bih.biPlanes = 1;
-    bih.biBitCount = 24;
-    bih.biCompression = BI_RGB;
-    fwrite(&bih, sizeof(bih), 1, fp);
+        // Write bitmap header
+        memset(&bfh, 0, sizeof(bfh));
+        bfh.bfType = 0x4D42; //"BM"
+        bfh.bfSize = N*N*3;
+        bfh.bfOffBits = sizeof(bfh) + sizeof(bih);
+        fwrite(&bfh, sizeof(bfh), 1, fp);
 
-    // Prepare 1st FFT pass
-    ret = gpu_fft_prepare(mb, log2_N, GPU_FFT_REV, N, fft_pass+0);
-    if (ret) {
-        return ret;
-    }
-    // Prepare 2nd FFT pass
-    ret = gpu_fft_prepare(mb, log2_N, GPU_FFT_REV, N, fft_pass+1);
-    if (ret) {
-        gpu_fft_release(fft_pass[0]);
-        return ret;
-    }
-    // Transpose from 1st pass output to 2nd pass input
-    ret = gpu_fft_trans_prepare(mb, fft_pass[0], fft_pass[1], &trans);
-    if (ret) {
+        // Write bitmap info
+        memset(&bih, 0, sizeof(bih));
+        bih.biSize = sizeof(bih);
+        bih.biWidth = N;
+        bih.biHeight = N;
+        bih.biPlanes = 1;
+        bih.biBitCount = 24;
+        bih.biCompression = BI_RGB;
+        fwrite(&bih, sizeof(bih), 1, fp);
+
+        // Prepare 1st FFT pass
+        ret = gpu_fft_prepare(mb, log2_N, GPU_FFT_REV, N, fft_pass+0);
+        if (ret) {
+            return ret;
+        }
+        // Prepare 2nd FFT pass
+        ret = gpu_fft_prepare(mb, log2_N, GPU_FFT_REV, N, fft_pass+1);
+        if (ret) {
+            gpu_fft_release(fft_pass[0]);
+            return ret;
+        }
+        // Transpose from 1st pass output to 2nd pass input
+        ret = gpu_fft_trans_prepare(mb, fft_pass[0], fft_pass[1], &trans);
+        if (ret) {
+            gpu_fft_release(fft_pass[0]);
+            gpu_fft_release(fft_pass[1]);
+            return ret;
+        }
+
+        // Clear input array
+        for (y=0; y<N; y++) {
+            row = GPU_FFT_ROW(fft_pass[0], in, y);
+            for (x=0; x<N; x++) row[x].re = row[x].im = 0;
+        }
+
+        // Setup input data
+        GPU_FFT_ROW(fft_pass[0], in,   2)[  2].re = 60;
+        GPU_FFT_ROW(fft_pass[0], in, N-2)[N-2].re = 60;
+
+        // ==> FFT() ==> T() ==> FFT() ==>
+        usleep(1); /* yield to OS */   t[0] = Microseconds();
+        gpu_fft_execute(fft_pass[0]);  t[1] = Microseconds();
+        gpu_fft_trans_execute(trans);  t[2] = Microseconds();
+        gpu_fft_execute(fft_pass[1]);  t[3] = Microseconds();
+
+        // Write output to bmp file
+        for (y=0; y<N; y++) {
+            row = GPU_FFT_ROW(fft_pass[1], out, y);
+            for (x=0; x<N; x++) {
+                fputc(128+row[x].re, fp); // blue
+                fputc(128+row[x].re, fp); // green
+                fputc(128+row[x].re, fp); // red
+            }
+        }
+
+        printf( "1st FFT   %6d usecs\n"
+                "Transpose %6d usecs\n"
+                "2nd FFT   %6d usecs\n",
+                t[3]-t[2], t[2]-t[1], t[1]-t[0]);
+
+        // Clean-up properly.  Videocore memory lost if not freed !
         gpu_fft_release(fft_pass[0]);
         gpu_fft_release(fft_pass[1]);
-        return ret;
+        gpu_fft_trans_release(trans);
     }
 
-    // Clear input array
-    for (y=0; y<N; y++) {
-        row = GPU_FFT_ROW(fft_pass[0], in, y);
-        for (x=0; x<N; x++) row[x].re = row[x].im = 0;
-    }
 
-    // Setup input data
-    GPU_FFT_ROW(fft_pass[0], in,   2)[  2].re = 60;
-    GPU_FFT_ROW(fft_pass[0], in, N-2)[N-2].re = 60;
-
-    // ==> FFT() ==> T() ==> FFT() ==>
-    usleep(1); /* yield to OS */   t[0] = Microseconds();
-    gpu_fft_execute(fft_pass[0]);  t[1] = Microseconds();
-    gpu_fft_trans_execute(trans);  t[2] = Microseconds();
-    gpu_fft_execute(fft_pass[1]);  t[3] = Microseconds();
-
-    // Write output to bmp file
-    for (y=0; y<N; y++) {
-        row = GPU_FFT_ROW(fft_pass[1], out, y);
-        for (x=0; x<N; x++) {
-            fputc(128+row[x].re, fp); // blue
-            fputc(128+row[x].re, fp); // green
-            fputc(128+row[x].re, fp); // red
-        }
-    }
-
-    printf( "1st FFT   %6d usecs\n"
-            "Transpose %6d usecs\n"
-            "2nd FFT   %6d usecs\n",
-            t[3]-t[2], t[2]-t[1], t[1]-t[0]);
-
-    // Clean-up properly.  Videocore memory lost if not freed !
-    gpu_fft_release(fft_pass[0]);
-    gpu_fft_release(fft_pass[1]);
-    gpu_fft_trans_release(trans);
 
     return 0;
+}
+
+unsigned Microseconds(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return ts.tv_sec*1000000 + ts.tv_nsec/1000;
 }
