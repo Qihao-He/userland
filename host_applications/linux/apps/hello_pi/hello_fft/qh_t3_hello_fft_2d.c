75937914fd58589b9c5cacf2dc4f9dd21072c90a
Qihao He <qi.he@maine.edu>
2018-02-26 21:23:44 -0500

    qh t3 2d debug

--- host_applications/linux/apps/hello_pi/hello_fft/qh_t3_hello_fft_2d.c
+++ host_applications/linux/apps/hello_pi/hello_fft/qh_t3_hello_fft_2d.c
@@ -184,7 +184,7 @@ int main(int argc, char *argv[]) {
                     fputc(128 + row[x].re, fp); // red
                 }
             }
-            printf("hello_fft_2d.bmp generated. Size:\n", N * N * 3);
+            printf("hello_fft_2d.bmp generated. Size%i:\n", bih.biSize);
         }
         // // print output
         // for (y = 0; y < N; y ++) {
